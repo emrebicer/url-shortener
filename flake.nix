404718f82415b10c33e5ac789de10461258b9125
Emre Bicer <oebicer@gmail.com>
2024-11-17 15:51:29 +0100

    improve flake usage

--- flake.nix
+++ flake.nix
@@ -3,38 +3,36 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-
-    let 
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-
-      devShells.${system}.default = pkgs.mkShellNoCC {
-        buildInputs = with pkgs; [
-          gcc9
-          cargo
-          rustc
-        ];
-
-      };
-
-      default = pkgs.rustPlatform.buildRustPackage {
-        pname = "url_shortener";
-        version = "0.1.0";
-        src = ./.;
-        cargoLock = {
-          lockFile = ./Cargo.lock;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: 
+      let 
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShellNoCC {
+          buildInputs = with pkgs; [
+            gcc9
+            cargo
+            rustc
+          ];
         };
-      };
-      
-      # Expose your program
-      apps.default = {
-        type = "app";
-        program = "${self.packages.default}/bin/url_shortener";
-      };
-      
-    };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "url_shortener";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+          };
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/url_shortener";
+        };
+
+      });
 }
+

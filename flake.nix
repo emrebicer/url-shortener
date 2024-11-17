f9188f985dc5dd77e6c38ae8a5c5166096a1b31a
Emre Bicer <oebicer@gmail.com>
2024-11-17 16:41:01 +0100

    update nixos module

--- flake.nix
+++ flake.nix
@@ -7,10 +7,13 @@
   };
 
   outputs = { self, nixpkgs, flake-utils }:
+    let 
+      lib = nixpkgs.lib;
+      pkgs = import nixpkgs;
+    in 
     flake-utils.lib.eachDefaultSystem (system:
       let 
         pkgs = import nixpkgs { inherit system; };
-        lib = nixpkgs.lib;
       in {
         devShells.default = pkgs.mkShellNoCC {
           buildInputs = with pkgs; [
@@ -33,42 +36,48 @@
           type = "app";
           program = "${self.packages.${system}.default}/bin/url_shortener";
         };
-
-        # NixOS module to enable the service
-        nixosModules.url-shortener = {
-          options.url-shortener = {
-            enable = lib.mkOption {
-              type = lib.types.bool;
-              default = false;
-              description = "Enable the URL shortener systemd service.";
-            };
-
-            host = lib.mkOption {
-              type = lib.types.str;
-              default = "127.0.0.1";
-              description = "The host the URL shortener will bind to.";
-            };
-
-            port = lib.mkOption {
-              type = lib.types.port;
-              default = 8000;
-              description = "The port the URL shortener will listen on.";
-            };
+      })
+    // {
+      # NixOS module to enable the service (system-independent)
+      nixosModules.url-shortener = {
+        options.url-shortener = {
+          enable = lib.mkOption {
+            type = lib.types.bool;
+            default = false;
+            description = "Enable the URL shortener systemd service.";
           };
 
-          config = lib.mkIf self.config.url-shortener.enable {
-            systemd.services.url-shortener = {
-              description = "URL Shortener Service";
-              after = [ "network.target" ];
-              wantedBy = [ "multi-user.target" ];
+          host = lib.mkOption {
+            type = lib.types.str;
+            default = "127.0.0.1";
+            description = "The host the URL shortener will bind to.";
+          };
 
-              serviceConfig = {
-                ExecStart = "${self.packages.${system}.default}/bin/url_shortener --host ${self.config.url-shortener.host} --port ${toString self.config.url-shortener.port}";
-                Restart = "always";
-              };
+          port = lib.mkOption {
+            type = lib.types.port;
+            default = 8000;
+            description = "The port the URL shortener will listen on.";
+          };
+        };
+
+        config = lib.mkIf self.config.url-shortener.enable {
+          systemd.services.url-shortener = {
+            description = "URL Shortener Service";
+            after = [ "network.target" ];
+            wantedBy = [ "multi-user.target" ];
+
+            serviceConfig = {
+              ExecStart = "${pkgs.rustPlatform.buildRustPackage {
+                pname = "url_shortener";
+                version = "0.1.0";
+                src = ./.;
+                cargoLock = { lockFile = ./Cargo.lock; };
+              }}/bin/url_shortener --host ${self.config.url-shortener.host} --port ${toString self.config.url-shortener.port}";
+              Restart = "always";
             };
           };
         };
-      });
+      };
+    };
 }
 

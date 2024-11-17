ff7c2e0c865e9369b344cd8178814f21235012dd
Emre Bicer <oebicer@gmail.com>
2024-11-17 15:38:39 +0100

    add arguments instead of env vars

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,33 @@ mod handlers;
 mod memory_database;
 mod url_manager;
 mod util;
+use clap::Parser;
 
 use axum::{routing::get, AddExtensionLayer, Router};
 use handlers::{not_found, root_get, root_post, short_url, web_get};
 use memory_database::MemoryDatabase;
-use std::env;
 use std::net::SocketAddr;
+use std::str::FromStr;
 use std::sync::Arc;
+
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(long, default_value_t = String::from("0.0.0.0"))]
+    host: String,
+
+    /// Number of times to greet
+    #[arg(long, default_value_t = 3000)]
+    port: u16,
+}
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let args = Args::parse();
     let db = Arc::new(MemoryDatabase::new());
 
     // Build the application with routes
@@ -22,14 +39,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .route("/404", get(not_found))
         .layer(AddExtensionLayer::new(db));
 
-    // Try to get the port from environment variables
-    let port = match env::var("PORT") {
-        Ok(port) => port.parse::<u16>()?,
-        Err(_) => 3000,
-    };
-
     // Start the server
-    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let addr = SocketAddr::from_str(&format!("{}:{}", args.host, args.port).to_string())
+        .expect("Failed to parse adress with given host and port");
+
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
